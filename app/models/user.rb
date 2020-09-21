2d502fefa4e636124a9ad43b6f62fae1584696b7
msy7822-ux <msy780022@gmail.com>
2020-09-21 14:57:53 +0000

    Made a basic User model and Secure Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :univ_name, presence: true
+    
+    # セキュアなパスワードの実装
+    has_secure_password
 end
