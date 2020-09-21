1c3ee882dc8ed37268f033a069bc2545d21fc40f
msy7822-ux <msy780022@gmail.com>
2020-09-21 12:23:08 +0000

    create users controller and signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
