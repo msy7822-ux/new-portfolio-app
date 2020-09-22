cba502d558dc574b44c6d014f2122dc5be450bd5
msy7822-ux <msy780022@gmail.com>
2020-09-22 09:12:35 +0000

    complete users index display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  # ログインしていないユーザーがアクセスできないアクション
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  # アカウントが異なるユーザーがアクセスできないアクション（ログインは完了済み）
   before_action :correct_user, only: [:edit, :update]
+  
+  
+  def index
+    @users = User.all
+  end 
   
   def new
     @user = User.new
