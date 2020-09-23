242f05a98fe034699f3a9653ccae8a557fd4d180
msy7822-ux <msy780022@gmail.com>
2020-09-23 09:34:00 +0000

    improve design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     # sessions helperの読み込み
     protect_from_forgery with: :exception
     include SessionsHelper
+    include UsersHelper
     
     def hello
        render html: 'Hello world' 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+    # ユーザーの投稿の活動内容は最初の１０文字しか表示しないメソッド
+    def num_of_words(text)
+        arr = text.split("")
+        result_text = []
+        if arr.size > 10
+            10.times{|n|
+                result_text << arr[n]
+            }
+            return result_text.join("")
+        else
+            return text
+        end
+    end
 end
